c18ad963934784aa082da8328f3a8c8f7c59aa56
Jacob Garrison <jgarrison1204@gmail.com>
2016-03-16 18:13:18 -0700

    Update

---
+++ lib/customer.rb
@@ -0,0 +1,7 @@
+class Customer
+
+  def initialize(options={})
+    @name = opitions[:name]
+  end
+
+end
--- lib/product.rb
+++ lib/product.rb
@@ -1,12 +1,44 @@
 class Product
+  attr_reader :title, :price, :stock
 
   @@products = []
 
   def initialize(options={})
-    @@products << self
+    @title = options[:title]
+    @price = options[:price]
+    @stock = options[:stock]
+    add_product
+  end
+
+  def in_stock?
+    #todo
   end
 
   def self.all
     @@products
+  end
+
+  #Overwrites the to_s in puts when printing @title
+  def to_s
+    "#{@title} #{@price} #{@stock}"
+  end
+
+  def self.find_by_title(stuff)
+    #todo
+    @@products.each do |find|
+      [find].include? stuff
+  end
+
+  def self.in_stock
+    #todo
+  end
+
+  private
+
+  def add_product
+    @@products << self
+    #@@products.each do |title|
+      #puts [title].something.include? @title
+    #end
   end
 end
