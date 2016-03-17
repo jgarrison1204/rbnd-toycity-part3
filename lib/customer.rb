f52085418d158a41b4151c670a830bcda8cece9d
Jacob Garrison <jgarrison1204@gmail.com>
2016-03-17 14:28:17 -0700

    Customers class functional

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,7 +1,34 @@
 class Customer
+  attr_reader :name
+
+  @@customers = []
 
   def initialize(options={})
-    @name = opitions[:name]
+    @name = options[:name]
+    add_customer
   end
 
+  def self.all
+    @@customers
+  end
+
+  def self.find_by_name(name)
+      @@customers.each do |cust|
+      search = cust.name.include? name
+      if search == true
+        return cust
+      end
+    end
+  end
+
+  private
+  def add_customer
+    @@customers.each do |cust|
+      dup_cust = cust.name.include? @name
+      if dup_cust == true
+        raise DuplicateCustomerError, "#{@name} already exists."
+      end
+    end
+    @@customers << self
+  end
 end
